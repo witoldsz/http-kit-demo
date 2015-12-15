22e8bfc79e271e60b71391b6b73700334b43bede
Witold Szczerba <witold.szczerba@currency-one.com>
2015-12-15 21:25:48 +0100

    compojure

--- src/http_kit_demo/core.clj
+++ src/http_kit_demo/core.clj
@@ -1,24 +1,27 @@
 (ns http-kit-demo.core
   (:gen-class)
-  (:require [org.httpkit.server :refer [run-server]]
+  (:require [org.httpkit.server :refer [run-server with-channel on-close on-receive websocket? send!]]
             [ring.util.response :refer [not-found]]
-            [ring.middleware.resource :refer [wrap-resource]]))
+            [ring.middleware.resource :refer [wrap-resource]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]))
 
-(defn handler [req] (not-found "not found"))
 
-(def app
-  (-> handler
-    (wrap-resource "public")
-    (fn [handler]
-      (fn [req]
-        (with-channel req channel ; get the channel
-          (if (websocket? channel) ; if you want to distinguish them
-            (on-receive channel (fn [data] (send! channel data)))
-            handler))))))
+(defn ws [req]
+  (with-channel req channel
+                (on-close channel (fn [status] (println "channel closed")))
+                (if (websocket? channel)
+                  (println "WebSocket channel")
+                  (println "HTTP channel"))
+                (on-receive channel (fn [data] (send! channel data)))))
 
-(defn -main
-  [& args]
+(defroutes all-routes
+           (GET "/ws" [] ws)
+           (route/resources "/static")
+           (route/not-found "<p>Page not found.</p>"))
+
+(defn -main [& args]
   (println "arguments:" args)
   (let [port (or (first args) "8080")]
-    (run-server app {:port (Integer/parseInt port)})
+    (run-server all-routes {:port (Integer/parseInt port)})
     (println "Hello, World at port" port)))
